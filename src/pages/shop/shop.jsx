332d03888ef62ab453eea1a563d5f7cd1d0375b8
Raghavendra2306 <91723638+Raghavendra2306@users.noreply.github.com>
2023-07-16 16:44:05 +0530

    Add files via upload
    1st commit

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { PRODUCTS } from '../../products';
+import { Product } from './product'; 
+
+export const Shop = () => {
+  return (
+    <div className='shop'>
+        <div className='shopTitle'>
+            <h1>Pedro Shop</h1>
+        </div>
+        <div className='products'>
+            {PRODUCTS.map((products) =>(
+                <Product  data={products} />
+            ))}
+        </div>
+    </div>
+  )
+}
